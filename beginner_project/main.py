7d54dbfaa6942d1f19bcecc37839b5665c6da08c
Tamas Hadhazy <tamas.hadhazy15@gmail.com>
2021-07-28 16:04:52 +0200

    update

--- beginner_project/main.py
+++ beginner_project/main.py
@@ -1,11 +1,12 @@
 # This Python file uses the following encoding: utf-8
 import os
 from pathlib import Path
-import sys
+import sys,json
+
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
-from PySide6.QtCore import QObject, Property, Signal
+from PySide6.QtCore import QObject, Property, Signal,Slot
 
 class Window_editor(QObject):
     def __init__(self):
@@ -17,22 +18,40 @@ class Window_editor(QObject):
         self.on_clicked_text = "Click!!"
         self.window_bg_color  = "grey"
 
+    @Slot(str,str,str,str,str)
+    def save_action(self,button_color,button_text,button_text_color,on_clicked_text,bg_color):
+        data = {
+            "button_color": button_color,
+            "button_text": button_text,
+            "button_text_color": button_text_color,
+            "on_clicked_text": on_clicked_text,
+            "bg_color": bg_color
+        }
 
 
-if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
-
-    engine = QQmlApplicationEngine()
-    editor = Window_editor()
+        with open("data.json", "w") as f:
+            json.dump(data,f)
 
 
-    context = engine.rootContext()
-    context.setContextProperty("Editor", editor)
+class Form:
+    def __init__(self):
+
+        self.app = QGuiApplication(sys.argv)
+
+        self.engine = QQmlApplicationEngine()
+
+
+
+        self.context = self.engine.rootContext()
+        self.editor = Window_editor()
+        self.context.setContextProperty("Editor", self.editor)
 
 
 
 
-    engine.load(os.fspath(Path(__file__).resolve().parent / "main.qml"))
-    if not engine.rootObjects():
-        sys.exit(-1)
-    sys.exit(app.exec())
+        self.engine.load(os.fspath(Path(__file__).resolve().parent / "main.qml"))
+        if not self.engine.rootObjects():
+            sys.exit(-1)
+        sys.exit(self.app.exec())
+if __name__ == '__main__':
+    Form()
